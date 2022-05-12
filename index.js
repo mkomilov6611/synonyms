d9f7d747614a80ff9237b8402c69349664822cc3
Mukhammadnazar Komilov <mkomilov6611@gmail.com>
2022-05-12 09:30:24 +0500

    optimized to not to cause stackoverflow, but not true yet

--- index.js
+++ index.js
@@ -1,54 +1,59 @@
-const fs = require('fs');
+const fs = require("fs");
 
 (() => {
-    fs.readFile('input.txt', "utf8", (err, data) => {
-        if (err) {
-            return console.error(`Read error: ${err}`)
-        }
+  fs.readFile("input.txt", "utf8", (err, data) => {
+    if (err) {
+      return console.error(`Read error: ${err}`);
+    }
 
-        const chunksMap = new Map()
+    const chunksMap = new Map();
 
-        let chunkSynonyms = []
-        let chunkQueries = []
+    let chunkSynonyms = [];
+    let chunkQueries = [];
 
-        let numberOccurence = 0
-        let indexLimitor = null;
+    let numberOccurence = 0;
+    let indexLimitor = null;
 
-        data.split('\r\n').forEach((item, index) => {
-            if (index === 0) {
-                return; // no logic for first line actually
-            }
+    data.split("\r\n").forEach((item, index) => {
+      if (index === 0) {
+        return; // no logic for first line actually
+      }
 
-            const currentItemAsNumber = Number.parseInt(item, 10)
-            const areSynonyms = () => numberOccurence % 2 !== 0 // reactive helper
+      const currentItemAsNumber = Number.parseInt(item, 10);
+      const areSynonyms = () => numberOccurence % 2 !== 0; // reactive helper
 
-            if (index > indexLimitor && chunkSynonyms.length && chunkQueries.length) {
-                chunksMap.set(chunkSynonyms, chunkQueries)
-                chunkSynonyms = []
-                chunkQueries = []
-            }
+      if (index > indexLimitor && chunkSynonyms.length && chunkQueries.length) {
+        chunksMap.set(chunkSynonyms, chunkQueries);
+        chunkSynonyms = [];
+        chunkQueries = [];
+      }
 
-            if (currentItemAsNumber) {
-                // its a number, so keep track of that much lines
-                numberOccurence++
-                indexLimitor = index + currentItemAsNumber;
-                return;
-            }
+      if (currentItemAsNumber) {
+        // its a number, so keep track of that much lines
+        numberOccurence++;
+        indexLimitor = index + currentItemAsNumber;
+        return;
+      }
 
-            // now we have string data, that is either part of synonyms or queries
-            if (areSynonyms()) {
-                return chunkSynonyms.push(item)
-            }
+      // now we have string data, that is either part of synonyms or queries
+      if (areSynonyms()) {
+        return chunkSynonyms.push(item);
+      }
 
-            chunkQueries.push(item)
-        });
+      chunkQueries.push(item);
+    });
 
-        const resultsText = require('./synonyms')(chunksMap);
+    const resultsText = require("./synonyms")(chunksMap);
 
-        fs.writeFile('output.txt', resultsText, "utf8", (err => {
-            if (err) {
-                return console.error(`Write error: ${err}`)
-            }
-        }))
-    })
-})()
+    fs.writeFile("output.txt", resultsText, "utf8", (err) => {
+      if (err) {
+        return console.error(`Write error: ${err}`);
+      }
+
+      const resultBuf = fs.readFileSync("output.txt");
+      const testBuf = fs.readFileSync("testOutput.txt");
+
+      console.log("Is true", resultBuf.equals(testBuf));
+    });
+  });
+})();
--- synonyms.js
+++ synonyms.js
@@ -1,60 +1,119 @@
 module.exports = function (chunksMap) {
-    let allChunksResults = []
+  let allChunksResults = [];
 
-    for (let [synonymsArray, queriesArray] of chunksMap) {
-        allChunksResults = [allChunksResults, ...markSynonyms(synonymsArray, queriesArray)]
-    }
+  for (let [synonymsArray, queriesArray] of chunksMap) {
+    allChunksResults = [
+      ...allChunksResults,
+      ...markSynonyms(synonymsArray, queriesArray),
+    ];
+  }
 
-    return allChunksResults.join('\r\n').trim();
-}
+  return allChunksResults.join("\r\n").trim();
+};
 
-const synonymsMap = new Map()
+const synonymsMap = new Map();
+let checkedMap = new Map();
 
 function markSynonyms(synonymsArray, queriesArray) {
-    const assumptions = []
-    const assumptionTypes = {
-        SYNONYMS: 'synonyms',
-        DIFFERENT: 'different'
+  checkedMap = new Map(); // reset the checked map for the new chunk
+
+  const assumptions = [];
+  const assumptionTypes = {
+    SYNONYMS: "synonyms",
+    DIFFERENT: "different",
+  };
+
+  // Fill the map(dictionary)
+  synonymsArray.forEach((synonymText) => {
+    const [left, right] = getNormalizedText(synonymText);
+
+    const adjacencyList = Array.isArray(synonymsMap.get(left))
+      ? synonymsMap.get(left)
+      : [];
+
+    // pipeline of optimizations during setting the dictionary
+    if (left === right) {
+      return;
     }
 
-    synonymsArray.forEach(synonymText => {
-        const [left, right] = getNormalizedText(synonymText)
-        synonymsMap.set(left, right)
-    });
+    if (synonymsMap.get(right)) {
+      if (synonymsMap.get(right).includes(left)) {
+        return;
+      }
+    }
 
+    adjacencyList.push(right);
 
-    queriesArray.forEach(queriesText => {
-        const [left, right] = getNormalizedText(queriesText)
+    return synonymsMap.set(left, adjacencyList);
+  });
 
-        if (checkQueries(left, right)) {
-            return assumptions.push(assumptionTypes.SYNONYMS)
-        }
+  // Search from the map
+  queriesArray.forEach((queriesText) => {
+    const [left, right] = getNormalizedText(queriesText);
 
-        return assumptions.push(assumptionTypes.DIFFERENT)
-    })
+    if (checkQueries(left, right)) {
+      return assumptions.push(assumptionTypes.SYNONYMS);
+    }
 
-    return assumptions
+    return assumptions.push(assumptionTypes.DIFFERENT);
+  });
+
+  return assumptions;
 }
 
 function checkQueries(left, right) {
-    if ((left.toLowerCase() === right.toLowerCase()) ||
-        (synonymsMap.get(left) === right) ||
-        (synonymsMap.get(right) === left)) {
+  if (left.toLowerCase() === right.toLowerCase()) {
+    return true;
+  }
 
-        return true
-    }
-
-    if (synonymsMap.get(left)) {
-        return checkQueries(synonymsMap.get(left), right)
-    }
-
-    return false
+  return (
+    checkAllAdjacents(left, right, true) ||
+    checkAllAdjacents(left, right, false)
+  );
 }
 
+function checkAllAdjacents(left, right, ofLeft) {
+  let tempLeft = null;
+  let tempRight = null;
+
+  if (ofLeft) {
+    tempLeft = left;
+    tempRight = right;
+  } else {
+    // also check the adjacents of right
+    tempLeft = right;
+    tempRight = left;
+  }
+
+  const adjacencyListOfLeft = synonymsMap.get(tempLeft);
+  if (adjacencyListOfLeft) {
+    // direct synonym
+    if (adjacencyListOfLeft.includes(tempRight)) {
+      return true;
+    }
+
+    const checkedList = checkedMap.get(tempLeft) || [];
+    checkedList.push(tempRight);
+
+    checkedMap.set(tempLeft, checkedList);
+
+    // indirect synonym
+    return adjacencyListOfLeft.some((probableSynonym) => {
+      if (checkedMap.get(tempLeft).includes(tempRight)) {
+        // already checked
+        return;
+      }
+
+      return checkQueries(probableSynonym, tempRight);
+    });
+  }
+
+  return false;
+}
 
 function getNormalizedText(text) {
-    const normalizedSynonymLeft = text.split(' ')[0].toLowerCase()
-    const normalizedSynonymRight = text.split(' ')[1].toLowerCase()
+  const normalizedSynonymLeft = text.split(" ")[0].toLowerCase();
+  const normalizedSynonymRight = text.split(" ")[1].toLowerCase();
 
-    return [normalizedSynonymLeft, normalizedSynonymRight]
+  return [normalizedSynonymLeft, normalizedSynonymRight];
 }
